b1ef52ab5abfa53feed3aee719e0b5606ca2a4e7
let-rec <toxtayevhamidulloh6997@gmail.com>
2025-06-20 20:42:12 +0500

    add: repl

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "rtest";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: 
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = import ./shell.nix {inherit pkgs;};
+        packages.default = pkgs.callPackage ./. {};
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+
+use clap::{ Parser, Subcommand};
+use std::io::{stdin};
+
+#[derive(Parser, Debug)]
+#[command(author,version,about,long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Get {
+        #[arg(required = true)]
+        arg: String,
+    },
+    Set {
+        key: i32,
+        value: String,
+        #[arg(required = false)]
+        is_true: bool
+    },
+    HelpCommand
+}
+
+
+pub fn get_something(arg: String) {
+    println!("Get fn: {}", arg);
+}
+pub fn set_something(key: i32, value: String, is_true: bool) {
+    if is_true {
+        println!("Set fn: {},{},{}", key, value, is_true)
+    }
+    println!("IS NOT TRUE MF")
+}
+pub fn show_commands() {
+    println!(r#"COMMANDS:
+get <KEY> - Gets the value of a given key and displays it. If no key given, retrieves all values and displays them.
+set <KEY> <VALUE> - Sets the value of a given key.
+    Flags: --is-true
+"#);
+}
+
+pub fn create_name() -> String {
+    let name: String = String::new();
+    name
+}
+
+#[tokio::main]
+async fn main() {
+
+    // let client  = reqwest::Client::new();
+    // let res = client.post("http://httpbin.org/post")
+    // .body("the exact body that is sent")
+    // .send()
+    // .await?;
+    // println!("body = {body:?}");
+
+    loop {
+        let mut buf = String::from(create_name());
+        stdin().read_line(&mut buf).expect("Couldn't parse stdin");
+        let line = buf.trim();
+        let args = shlex::split(line).ok_or("error: invalid quoting").unwrap();
+
+        println!("ITS ARGS: {:?}", args);
+        match Args::try_parse_from(args.iter()).map_err(|e| e.to_string()) {
+            Ok(cli) =>{
+                println!("THIS IS CLI COMMAND: {:?}", cli);
+                match cli.cmd {
+                    Commands::Get { arg } => get_something(arg),
+                    Commands::Set { key, value, is_true } => set_something(key, value, is_true),
+                    Commands::HelpCommand => show_commands(),
+                }
+            }
+            Err(e) => println!("Not valid command: {}", e)
+        };
+    }
+
+}
+
+
+
+
+// -- LEARN this --
+// #[derive(Default)]
+// struct Foo{}
+
+
+
+// -- JUST READ FILE WITH CLI --
+/*
+use std::env;
+use std::process;
+use funcs::{Config, run};
+
+mod funcs;
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arg: {}" , err);
+        process::exit(1);
+    });
+    
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+*/
+
+
+
+/*
+let args = Cli::parse();
+let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+for line in content.lines() {
+    if line.contains(&args.pattern) {
+        print!("Line: {}", line);
+    }
+}
+// println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+*/
+
+/*
+// #[arg(short = 'o', long = "output")]
+#[derive(Debug, Parser)]
+#[clap(author,version,about)]
+struct Cli{
+    pattern: String,
+    path: std::path::PathBuf,
+}
+*/
